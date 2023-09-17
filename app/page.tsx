ceb672cd91464f89443efb70208a536eaa9cb973
Ney Nascimento <135929048+ney-nascimento@users.noreply.github.com>
2023-09-17 16:09:10 -0300

    Header-Next-Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,33 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <header className=' bg-blue-500  w-screen flex place-items-center p-6 justify-between shadow-lg rounded-bl-lg rounded-br-lg fixed'>
-      <h1 className='text-white text-4xl' >HelloWorld</h1>
+    <header className='bg-blue-700 w-screen flex place-items-center p-0 justify-between shadow-lg rounded-bl-lg rounded-br-lg fixed'>
+      <h1 className='text-gray-600 text-6xl font-bold font-style: italic'>HTC BY SOLUCTION</h1>
       <nav className='text-white flex flex-wrap p-6 items-center justify-between'>
         <ul className='flex gap-10 px-3 py-1  cursor-pointer transition'>
           <li className='px-3 py-1 hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition'>
-            <a href="">exemmplo</a>
+            <a href="" className='text-black text-2xl font-bold'>Entre em contato</a>
           </li>
           <li className='px-3 py-1 hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition'>
-            <a href="">exemmplo</a>
+            <a href="" className='text-black text-2xl font-bold'>Conecte-se</a>
           </li>
           <li className='px-3 py-1 hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition'>
-            <a href="">exemmplo</a>
+            <a href="" className='text-black text-2xl font-bold'>Sobre</a>
           </li>
         </ul>
-        <button className='group/enter flex gap-2 ml-10 rounded-2xl border border-black shadow-lg px-3 py-1 font-light'>login</button>
+        <button className='group/enter flex gap-2 ml-10 rounded-2xl border border-black shadow-lg px-3 py-1 font-light'>ENTER</button>
       </nav>
-
+     
     </header>
+
+    
   )
 
+  }
 
-}
+  {
+    (
+      <div class="h-full w-[80%] bg-gradient-to-r from-black to-transparent flex pl-10 text-9xl text-white font-bold place-items-center"><h1>
+        <span class="text-detail">Nosso</span> produto <br>Systema Administrativo</h1></div>
+    )
+  }
